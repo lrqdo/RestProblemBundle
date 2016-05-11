26d8139ce0722ddccc0498b544b979ae37d2e9a6
Kim Laï Trinh <kimlai@lrqdo.fr>
2016-05-11 10:07:31 +0200

    Enable logging exceptions
    The exception listener was not compatible with Symfony's exception
    listener: they both set a Response, so they both stop the exception
    event propagation.
    
    With this change, the listener first stores a reference to the thrown
    exception, then lets Symfony's exception listener log the exception and
    rendering the error page (unfortunately this is done in the same
    listener, an issue has been opened
    [here](https://github.com/symfony/symfony/issues/18552)). Then the
    listener waits for a response event, checks the status code of the
    response, and if it's an error, it's returns the RestProblem custom
    response as it did before, thus keeping backward compatibility.

--- src/Alterway/Bundle/RestProblemBundle/EventListener/ExceptionListener.php
+++ src/Alterway/Bundle/RestProblemBundle/EventListener/ExceptionListener.php
@@ -5,10 +5,12 @@ namespace Alterway\Bundle\RestProblemBundle\EventListener;
 use Alterway\Bundle\RestProblemBundle\Problem\Exception;
 use Alterway\Bundle\RestProblemBundle\Response\ProblemResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
 class ExceptionListener
 {
     private $debugMode;
+    private $exception;
 
     public function __construct($debugMode)
     {
@@ -17,6 +19,13 @@ class ExceptionListener
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        $event->setResponse(new ProblemResponse(new Exception($event->getException(), $this->debugMode)));
+        $this->exception = $event->getException();
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if ($event->getResponse()->isServerError()) {
+            $event->setResponse(new ProblemResponse(new Exception($this->exception, $this->debugMode)));
+        }
     }
 }
